14cf70387e82f8a7fef4543142b159892bf655e0
giulio-deangelis <giuliodeangelis95@gmail.com>
2020-07-28 16:43:22 +0000

    Master-detail example

--- test-mta-ui/webapp/controller/Home.controller.js
+++ test-mta-ui/webapp/controller/Home.controller.js
@@ -11,88 +11,97 @@ sap.ui.define([
   "use strict";
   return Controller.extend("test.test-mta-ui.controller.Home", {
 
-    onInit: function () {},
-    
-    onAfterRendering: function() {
-      this._table = this.getView().byId("table");
-      this._tableTitle = this.getView().byId("tableTitle");
-      this._model = this.getView().getModel("oDataModel");
-      this._top = 100;
-      this._skip = 0;
-      
-      if (!this._model)
-        throw Error("oDataModel not found");
-        
-      this._model.read("/Orders", {
-        urlParameters: { "$top": this._top },
-        success: function (oData) {
-          const model = new JSONModel(oData);
-          this.getView().setModel(model, "orders");
-          this._updateTitle();
-        }.bind(this)
+    onRowPress: function (ev) {
+    // this.byId("split").toDetail(this.createId("detail"));
+      this.byId("detail").scrollTo(0, 0);
+      this.getView().bindElement({
+        path: ev.getSource().getBindingContext("oDataModel").getPath(),
+        model: "oDataModel"
       });
-    },
-
-    onProductsButtonPress: function (ev) {
-      Navigator.navigate(this, ev);
-    },
-    
-    onPrevButtonPress: function (ev) {
-      this._loadPage(ev.getSource(), true);
-    },
-
-    onNextButtonPress: function (ev) {
-      this._loadPage(ev.getSource());
-    },
-
-    onSearch: function (ev) {
-      const query = ev.getParameter("query");
-      const filter = query ? new Filter({
-        filters: [
-          new Filter("CustomerID", FilterOperator.Contains, query),
-          new Filter("EmployeeID", FilterOperator.EQ, query),
-          new Filter("OrderID", FilterOperator.EQ, query)
-        ],
-        and: false
-      }) : [];
-      this._table.getBinding("items").filter(filter);
-    },
-
-    _loadPage: function (pressedButton, prev) {
-      if (prev) {
-        const skip = this._skip - this._top;
-        if (skip >= 0)
-          this._skip = skip;
-        else
-          return;
-      } else {
-        this._skip += this._top;
-      }
-      this._model.read("/Orders", {
-        urlParameters: {
-          "$top": this._top,
-          "$skip": this._skip
-        },
-        success: function (oData) {
-          const model = this.getView().getModel("orders");
-          model.setData(oData);
-          model.refresh(true);
-          this._updateTitle();
-          this._scrollTopIfNeeded(pressedButton);
-        }.bind(this)
-      });
-    },
-
-    _updateTitle: function () {
-      const title = this.getView().getModel("i18n").getProperty("orderTableTitle");
-      const low = this._skip;
-      const high = this._skip + this._top;
-      this._tableTitle.setText(title + " (" + low + " - " + high + ")");
-    },
-
-    _scrollTopIfNeeded: function (pressedButton) {
-      if (pressedButton.getId().includes("bottom"))
-        this.byId("page").scrollTo(0, 0);
     }
+
+    // onInit: function () {},
+
+    // onAfterRendering: function() {
+    //   this._table = this.getView().byId("table");
+    //   this._tableTitle = this.getView().byId("tableTitle");
+    //   this._model = this.getView().getModel("oDataModel");
+    //   this._top = 100;
+    //   this._skip = 0;
+
+    //   if (!this._model)
+    //     throw Error("oDataModel not found");
+
+    //   this._model.read("/Orders", {
+    //     urlParameters: { "$top": this._top },
+    //     success: function (oData) {
+    //       const model = new JSONModel(oData);
+    //       this.getView().setModel(model, "orders");
+    //       this._updateTitle();
+    //     }.bind(this)
+    //   });
+    // },
+
+    // onProductsButtonPress: function (ev) {
+    //   Navigator.navigate(this, ev);
+    // },
+
+    // onPrevButtonPress: function (ev) {
+    //   this._loadPage(ev.getSource(), true);
+    // },
+
+    // onNextButtonPress: function (ev) {
+    //   this._loadPage(ev.getSource());
+    // },
+
+    // onSearch: function (ev) {
+    //   const query = ev.getParameter("query");
+    //   const filter = query ? new Filter({
+    //     filters: [
+    //       new Filter("CustomerID", FilterOperator.Contains, query),
+    //       new Filter("EmployeeID", FilterOperator.EQ, query),
+    //       new Filter("OrderID", FilterOperator.EQ, query)
+    //     ],
+    //     and: false
+    //   }) : [];
+    //   this._table.getBinding("items").filter(filter);
+    // },
+
+    // _loadPage: function (pressedButton, prev) {
+    //   if (prev) {
+    //     const skip = this._skip - this._top;
+    //     if (skip >= 0)
+    //       this._skip = skip;
+    //     else
+    //       return;
+    //   } else {
+    //     this._skip += this._top;
+    //   }
+    //   this._model.read("/Orders", {
+    //     urlParameters: {
+    //       "$top": this._top,
+    //       "$skip": this._skip
+    //     },
+    //     success: function (oData) {
+    //       const model = this.getView().getModel("orders");
+    //       model.setData(oData);
+    //       model.refresh(true);
+    //       this._updateTitle();
+    //       this._scrollTopIfNeeded(pressedButton);
+    //     }.bind(this)
+    //   });
+    // },
+
+    // _updateTitle: function () {
+    //   const title = this.getView().getModel("i18n").getProperty("orderTableTitle");
+    //   const low = this._skip;
+    //   const high = this._skip + this._top;
+    //   this._tableTitle.setText(title + " (" + low + " - " + high + ")");
+    // },
+
+    // _scrollTopIfNeeded: function (pressedButton) {
+    //   if (pressedButton.getId().includes("bottom"))
+    //     this.byId("page").scrollTo(0, 0);
+    // }
   });
 });
